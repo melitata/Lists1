26462fd3b91ea67ae6640f47244e30ff8e29fd99
Melisa Kılıç <mk_i3@hotmail.com>
2024-11-04 20:53:27 +0300

    Proje dosyası ekle.

---
+++ Lists1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> guests = new List<string>
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Bergen",
+            "Müslüm Baba",
+            "Murat Boz",
+            "Tupac",
+            "Tarkan"
+        };
+        Console.WriteLine("Davetliler Listesi:");
+        foreach (string guest in guests)
+        {
+            Console.WriteLine(guest);
+        }
+    }
+}
